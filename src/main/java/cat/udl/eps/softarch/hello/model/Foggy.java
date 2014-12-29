7fdb861477848a34b6835f1ebdc9aab477eb3fdc
GenisVillarte <villartexr@gmail.com>
2014-12-29 18:00:38 +0100

    models creats

---
+++ src/main/java/cat/udl/eps/softarch/hello/model/Foggy.java
@@ -0,0 +1,17 @@
+package cat.udl.eps.softarch.hello.model;
+
+import javax.persistence.Entity;
+import java.util.Observable;
+
+/**
+ * Created by http://rhizomik.net/~roberto/
+ */
+@Entity
+public class Foggy extends Observable implements Weather {
+
+
+    public static final Foggy foggy = new Foggy();
+
+    private Foggy(){}
+
+}
---
+++ src/main/java/cat/udl/eps/softarch/hello/model/Rain.java
@@ -0,0 +1,17 @@
+package cat.udl.eps.softarch.hello.model;
+
+import javax.persistence.Entity;
+import java.util.Observable;
+
+/**
+ * Created by http://rhizomik.net/~roberto/
+ */
+@Entity
+public class Rain extends Observable implements Weather {
+
+
+    public static final Rain rain = new Rain();
+
+    private Rain(){}
+
+}
